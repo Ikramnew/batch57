693ab12a3d4094c5a3911802abc1e47a71fb9842
unknown <ikramjundullah96@gmail.com>
2024-09-19 02:28:27 +0700

    add v.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false, // Nonaktifkan logging jika tidak diperlukan
 });
 
 module.exports = sequelize;
